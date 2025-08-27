324e86de8c5376c64b58be78020f4a6abb1133d6
nicoluvas <nicolaspoficial@gmail.com>
2025-08-26 21:06:37 -0300

    base funct

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,31 @@ import { useEffect, useRef, useState } from 'react'
 
 function App() {
   const [color, setColor] = useState('#242424')
+  const [p, setP] = useState('click anywhere to start')
+  const [active, setActive] = useState(false)
   
   function handleClick() {
-    console.log('clicked')
     setColor('#1a130fff')
+    setP('wait to green')
+
+    let rand = Math.floor(Math.random() * (20 - 5) + 5)
+    const interval = setInterval(() => {
+      if (rand > 0) {
+        rand--
+        console.log('baixo: ' + rand)
+      } else {
+        setActive(false)
+        clearInterval(interval)
+        setColor('#121b15ff')
+        setP(`its green`)
+      }
+    }, 100)
+    
   }
 
   return (
     <div className='main' onClick={handleClick} style={{ backgroundColor: color }}>
-      aaaaa
+      <p>{p}</p>
     </div>
   )
 }
