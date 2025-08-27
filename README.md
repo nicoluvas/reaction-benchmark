030d3e008b310944255649aeee0b23b20d95a39c
nicoluvas <nicolaspoficial@gmail.com>
2025-08-26 21:28:29 -0300

    check if valid click

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # reaction benchmark
+
+---
+
 ## color palette
 - #242424
 - #1a130fff
@@ -6,6 +9,10 @@
 
 ## logic
 1. click first time
-2. runs decrescent random timer
-3. triggers clickable canva
-4. calculates amout of time (ms) after triggering
+1. set as active 
+1. runs decrescent random timer
+1. triggers clickable canva
+1. user clicks
+1. calculates amout of time (ms) after triggering
+1. set as inactive
+1. user clicks again...
--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,36 @@ function App() {
   const [color, setColor] = useState('#242424')
   const [p, setP] = useState('click anywhere to start')
   const [active, setActive] = useState(false)
-  
-  function handleClick() {
-    setColor('#1a130fff')
-    setP('wait to green')
 
-    let rand = Math.floor(Math.random() * (20 - 5) + 5)
-    const interval = setInterval(() => {
-      if (rand > 0) {
-        rand--
-        console.log('baixo: ' + rand)
-      } else {
-        setActive(false)
-        clearInterval(interval)
-        setColor('#121b15ff')
-        setP(`its green`)
+  function handleClick() {
+    if (active) {
+
+      if (color == '#1a130fff') {
+        setP('too soon')
+      } else if (color == '#121b15ff') {
+        setP('yaaay on green')
       }
-    }, 100)
-    
+
+    } else {
+
+      setActive(true)
+      setColor('#1a130fff')
+      setP('wait to green')
+
+      let rand = Math.floor(Math.random() * (20 - 5) + 5)
+      const interval = setInterval(() => {
+        if (rand > 0) {
+          rand--
+          console.log('baixo: ' + rand)
+        } else {
+          setActive(false)
+          clearInterval(interval)
+          setColor('#121b15ff')
+          setP(`its green`)
+        }
+      }, 100)
+    }
+
   }
 
   return (
